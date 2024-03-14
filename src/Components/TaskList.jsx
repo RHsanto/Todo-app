cbf1c10a9c0ae585f2f456fbf28a53cd448864ec
RHsanto <rakibulsanto789@gmail.com>
2024-03-14 15:31:57 +0600

    intial setup done

---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,11 @@
+
+
+const TaskList = () => {
+  return (
+    <div>
+      <h1>TaskList</h1>
+    </div>
+  );
+};
+
+export default TaskList;
---
+++ src/Redux/Slice/taskSlice.jsx
@@ -0,0 +1,11 @@
+
+
+const taskSlice = () => {
+  return (
+    <div>
+      <h1>taskSlice</h1>
+    </div>
+  );
+};
+
+export default taskSlice;
